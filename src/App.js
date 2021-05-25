afa5f35ada2758cdb2404881ced468ba9861f987
vasanthmaxwell <vasanthgta@gmail.com>
2021-05-25 17:28:26 +0530

    filter dynamic added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,102 @@
-import React from 'react';
-import Card from './components/card';
+import React, { useEffect, useState } from "react";
+import Card from "./components/card";
+import Star from "./assets/star.png";
+import axios from "axios";
 function App() {
+  const [page, setPage] = useState(1);
+  const [data, setdata] = useState([]);
+  const [all, setAll] = useState([]);
+  const [filters, setFilters] = useState([]);
+  const [filtervalue, setFilterValue] = useState("All");
+  useEffect(() => {
+    axios
+      .get(
+        `https://script.google.com/macros/s/AKfycbyS78rJrCu8JKkSw3qt43c047o1uMSM74dy0iYOP0dKO7sAlM0u1TAwCKjTbqBTzK9Ztw/exec?limit=${50}&offset=${page}`
+      )
+      .then((lol) => {
+        setdata([...data, ...lol.data[0].data]);
+        setAll([...data, ...lol.data[0].data]);
+        setFilters(lol.data[0].data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [page]);
+
+  const filterCards = () => {
+    if (filtervalue === "All") {
+      console.log("All");
+      setAll(data);
+    } else {
+      const dataValue = data.filter((ch) => ch.Service === filtervalue);
+      setAll(dataValue);
+    }
+  };
+  useEffect(() => {
+    filterCards();
+  }, [filtervalue, page]);
+  console.log(data.length);
+
+  const scrollToend = () => {
+    setPage(page + 50);
+    console.log(page);
+  };
+  console.log(page);
+  window.onscroll = function () {
+    if (
+      Math.ceil(window.innerHeight + document.documentElement.scrollTop) ===
+      document.documentElement.offsetHeight
+    ) {
+      console.log("scrolling");
+      scrollToend();
+    }
+  };
+  console.log(filters.length);
+  const [dropdownlist, setDropdownlist] = useState([]);
+  for (let i = 0; i < filters.length; i++) {
+    const filterTitle = filters[i].Service;
+    if (dropdownlist.findIndex((ch) => ch.Service === filterTitle) === -1) {
+      dropdownlist.push({ Service: filterTitle });
+    }
+  }
+
+  console.log(dropdownlist);
   return (
-    <div className='App'>
-      <Card></Card>
-    </div>
+    <>
+      <div className="filter-bar">
+        <button>All</button>
+        {dropdownlist.map((ns) => {
+          return (
+            <button onClick={() => setFilterValue(ns.Service)}>
+              {ns.Service}
+            </button>
+          );
+        })}
+      </div>
+      <div className="App">
+        {all.map((ch) => {
+          const Ratings = [];
+          const star = ch.Rating.slice(0, 1);
+          for (let i = 0; i < parseInt(star); i++) {
+            Ratings.push(<img src={Star} alt="" />);
+          }
+          return (
+            <div className="col">
+              <Card
+                Photo={ch.Photo}
+                Ratings={Ratings}
+                Name={ch.Name}
+                Position={ch.Position}
+                Review={ch.Review}
+                Title={ch.Title}
+                Companylogo={ch.Companylogo}
+                Service={ch.Service}
+              ></Card>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
--- src/components/card.js
+++ src/components/card.js
@@ -1,130 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import Star from '../assets/star.png';
-import axios from 'axios';
+import React, { useState } from "react";
 
-const Card = () => {
-  const [page, setPage] = useState(1);
-  const [data, setdata] = useState([]);
-  const [all, setAll] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const params = new URLSearchParams(window.location.search);
-  const [readMore, setReadMore] = useState();
-  useEffect(() => {
-    axios
-      .get(
-        `https://script.google.com/macros/s/AKfycbyS78rJrCu8JKkSw3qt43c047o1uMSM74dy0iYOP0dKO7sAlM0u1TAwCKjTbqBTzK9Ztw/exec?limit=50&offset=${page.toString}`
-      )
-      .then((lol) => {
-        setdata([...data, ...lol.data[0].data]);
-
-        setAll([...data, ...lol.data[0].data]);
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [page]);
-  // data.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
-  const filteredData = (filtervalue) => {
-    if (filtervalue == 'All') {
-      console.log('All');
-      setAll(data);
-      console.log(data);
-    } else {
-      const dataValue = data.filter((ch) => ch.Service === filtervalue);
-      setAll(dataValue);
-    }
-  };
-  console.log(data.length);
-
-  const scrollToend = () => {
-    setPage(page + 50);
-    console.log(page);
-  };
-  window.onscroll = function () {
-    if (
-      Math.ceil(window.innerHeight + document.documentElement.scrollTop) ===
-      document.documentElement.offsetHeight
-    ) {
-      console.log('scrolling');
-      scrollToend();
-    }
-    // console.log(
-    //   Math.ceil(window.innerHeight + document.documentElement.scrollTop),
-    //   document.documentElement.offsetHeight
-    // );
-  };
-  console.log(data);
+const Card = ({
+  Photo,
+  Ratings,
+  Name,
+  Position,
+  Title,
+  Review,
+  Companylogo,
+  Service,
+}) => {
+  const [readMore, setReadMore] = useState(false);
 
   return (
-    <>
-      <div className='filter-bar'>
-        <button onClick={() => filteredData('All')}>All</button>
-        <button onClick={() => filteredData('Health Insurance')}>Health</button>
-        <button onClick={() => filteredData('Car Insurance')}>Motor</button>
-        <button onClick={() => filteredData('Personal Insurance')}>
-          Marine
-        </button>
-        <button onClick={() => filteredData('Personal Insurance')}>
-          Travel
-        </button>
-      </div>
-      <div className='no-view'>
-        {all &&
-          all.map((ch, value) => {
-            const Ratings = [];
-            const star = ch.Rating.slice(0, 1);
-            for (let i = 0; i < parseInt(star); i++) {
-              Ratings.push(<img src={Star} alt='' />);
-            }
-            let read = ch.Review.slice(0, 100);
-            const ReadMore = (index) => {
-              console.log('isClicked');
-              read = ch.Review;
-              setReadMore(index);
-            };
-            return (
-              <div className='col'>
-                <div className='endorsal-card'>
-                  {ch.Photo === '' ? (
-                    ''
-                  ) : (
-                    <img src={ch.Photo} alt='' className='avatar' />
-                  )}
-                  <div className='rating'>{Ratings}</div>
-                  <h3 className='name'>{ch.Name}</h3>
-                  <h4 className='place'>{ch.Position}</h4>
-                  <h3>{ch.Title}</h3>
-                  {ch.Review === '' ? (
-                    ''
-                  ) : (
-                    <p>
-                      {readMore === value ? ch.Review : read}
-                      <span>
-                        {readMore === value ? (
-                          ''
-                        ) : read.length < 100 ? (
-                          ''
-                        ) : (
-                          <button onClick={() => ReadMore(value)}>
-                            Read More
-                          </button>
-                        )}
-                      </span>
-                    </p>
-                  )}
-                  {ch.Companylogo === '' ? (
-                    ''
-                  ) : (
-                    <img src={ch.Companylogo} alt='' className='logo' />
-                  )}
-                  <p className='review'>{`Review for ${ch.Service}`}</p>
-                </div>
-              </div>
-            );
-          })}
-      </div>
-    </>
+    <div className="endorsal-card">
+      {Photo === "" ? "" : <img src={Photo} alt="" className="avatar" />}
+      <div className="rating">{Ratings}</div>
+      <h3 className="name">{Name}</h3>
+      <h4 className="place">{Position}</h4>
+      <h3>{Title}</h3>
+      {Review === "" ? (
+        ""
+      ) : (
+        <p>
+          {readMore === false ? Review.slice(0, 100) : Review}
+          <span>
+            {Review.length < 100 ? (
+              ""
+            ) : (
+              <button onClick={() => setReadMore(true)}>Read More</button>
+            )}
+          </span>
+        </p>
+      )}
+      {Companylogo === "" ? (
+        ""
+      ) : (
+        <img src={Companylogo} alt="" className="logo" />
+      )}
+      <p className="review">{`Review for ${Service}`}</p>
+    </div>
   );
 };
 
